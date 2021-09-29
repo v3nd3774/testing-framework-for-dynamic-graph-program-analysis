7f4b232ced391b69a5cd33ef3e17edbe23e7ca78
Josue Caraballo <josue.caraballo@mavs.uta.edu>
2021-09-29 15:14:53 -0500

    fix(failing test): now it's fixed

--- plugin/src/main/java/uta2218/cse6324/s001/GraphUtility.java
+++ plugin/src/main/java/uta2218/cse6324/s001/GraphUtility.java
@@ -3,12 +3,19 @@ package uta2218.cse6324.s001;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import java.util.HashMap;
+import java.util.Set;
 
 class GraphUtility {
     protected static boolean equals(
-        Graph<HashMap<String, String>, DefaultEdge> a,
-        Graph<HashMap<String, String>, DefaultEdge> b
+        Graph<HashMap<String, String>, DefaultEdge> Ga,
+        Graph<HashMap<String, String>, DefaultEdge> Gb
     ) {
-        return true;
+      Set<HashMap<String, String>> Va = Ga.vertexSet();
+      Set<HashMap<String, String>> Vb = Gb.vertexSet();
+
+      Set<DefaultEdge> Ea = Ga.edgeSet();
+      Set<DefaultEdge> Eb = Gb.edgeSet();
+
+      return Va.equals(Vb) && Ea.equals(Eb);
       }
 }
